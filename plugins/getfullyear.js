8ea129c29c1996e82cf8e50757b63d89fe6d57c1
xgqfrms <xgqfrms@outlook.com>
2017-04-04 15:37:24 +0800

    copyright 2017-2050

--- plugins/getfullyear.js
+++ plugins/getfullyear.js
@@ -1,3 +1,39 @@
+/**
+ * xgqfrms 2017.01.01
+ * @copyright xgqfrms 2017-2050
+ * @version 1.1.1
+ * @link https://cdn.xgqfrms.xyz/plugins/getfullyear.js
+ */
+
+const copyRightYear = () => {
+    let copyYear = Symbol();
+    copyYear = new Date().getFullYear();
+    const spanYear = document.getElementById('spanYear');
+    spanYear.style.background = "yellow";
+    spanYear.style.color = "#000";
+    spanYear.innerHTML = `Copyright &copy; ${copyYear} - 2050 xgqfrms`;
+    return spanYear;
+}
+window.onload = copyRightYear();
+
+
+/*
+<span id="spanYear"></span>
+https://cdn.xgqfrms.xyz/plugins/getfullyear.js
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 let copyyear = new Date().getFullYear();
 
@@ -11,15 +47,6 @@ copyRightYear();
 */
 
 
-
-
-const copyRightYear = () => {
-    let copyYear = Symbol();
-    copyYear = new Date().getFullYear();
-    const spanYear = document.getElementById('spanYear')
-    return spanYear.innerHTML = `Copyright &copy; ${copyYear} xgqfrms`;
-}
-window.onload = copyRightYear();
 
 
 
