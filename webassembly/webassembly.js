8d6a1392cae452f4fea2bf362da3f80423eb1250
xgqfrms <xgqfrms@outlook.com>
2017-06-08 17:05:34 +0800

    update

--- webassembly/webassembly.js
+++ webassembly/webassembly.js
@@ -3,12 +3,12 @@
 
 
 (module
-  (export "sqrt" (func $sqrt))
-  (func $sqrt
-    (param $num f32)
-    (result f32)
-    (f32.sqrt (get_local $num))
-  )
+    (export "sqrt" (func $sqrt))
+    (func $sqrt
+        (param $num f32)
+        (result f32)
+        (f32.sqrt (get_local $num))
+    )
 )
 
 */
@@ -16,9 +16,7 @@
 
 (() => {
     const WebA_URL = `https://cdn.xgqfrms.xyz/webassembly/xgqfrms.wasm`;
-    const ThrowErrorInfo = () => {
-        throw new Error(`fetch WASM failed!`);
-    };
+    const ThrowErrorInfo = () => {throw new Error(`fetch WASM failed!`)};
     fetch(`${WebA_URL}`)
     .then(res => res.ok ? res.arrayBuffer() : ThrowErrorInfo())
     .then(bytes => WebAssembly.compile(bytes))
