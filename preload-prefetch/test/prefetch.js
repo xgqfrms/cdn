3d7132e788e90509cbb5c6e3641215ed5c30fd03
xgqfrms <xgqfrms@outlook.com>
2020-05-20 01:27:34 +0800

    Add files via upload

---
+++ preload-prefetch/test/prefetch.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-05-19
+ * @modified
+ *
+ * @description prefetch
+ * @augments
+ * @example
+ * @link
+ *
+ */
+
+// const log = console.log;
+
+log(`prefetch.js`)
