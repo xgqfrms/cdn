77291cc08f03ecf6ece019af1a85ea939fc09af1
xgqfrms <xgqfrms@outlook.com>
2020-08-22 22:07:34 +0800

    Update seo-flag.js

--- plugins/seo-flag.js
+++ plugins/seo-flag.js
@@ -32,7 +32,7 @@ const SEO_FALG = () => {
     const a = document.querySelector(`[data-flagcounter="a"]`);
     if(a && img && !window.SEO_FALG_FINISHED) {
       log(`❓flagcounter.com trying ${counter} times`);
-      a.href = `https://s11.flagcounter.com/count2/QIXi`;
+      a.href = `https://info.flagcounter.com/QIXi`;
       img.src = `https://s11.flagcounter.com/count2/QIXi/bg_000000/txt_00FF00/border_FF00FF/columns_3/maxflags_12/viewers_0/labels_1/pageviews_1/flags_0/percent_1/`;
       window.SEO_FALG_FINISHED = true;
     } else {
