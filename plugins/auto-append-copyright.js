d6ac4bcaa7f9cbeeca981bce28b9cbc316b69ac0
xgqfrms <xgqfrms@outlook.com>
2021-05-29 13:01:38 +0800

    Update auto-append-copyright.js

--- plugins/auto-append-copyright.js
+++ plugins/auto-append-copyright.js
@@ -29,15 +29,15 @@ document.body.oncopy = event => {
     // 复制很少的文字则不添加版权信息，超过一定长度的文字就添加版权信息
     if (copyTexts.length > 7) {
         copyrightTexts = `
-            ${copyTexts}
-            作者：xgqfrms
-            链接：${window.location.href ?? 'https://www.cnblogs.com/xgqfrms/'}
-            来源：${window.location.origin}
-            著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-                        ©xgqfrms 2012-2021
-            ${window.location.host} 发布文章使用：只允许注册用户才可以访问！
-                原创文章，版权所有©️xgqfrms, 禁止转载 🈲️，侵权必究⚠️！
-        `;
+${copyTexts}
+作者：xgqfrms
+链接：${window.location.href ?? 'https://www.cnblogs.com/xgqfrms/'}
+来源：${window.location.origin}
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+            ©xgqfrms 2012-2021
+${window.location.host} 发布文章使用：只允许注册用户才可以访问！
+     原创文章，版权所有©️xgqfrms, 禁止转载 🈲️，侵权必究⚠️！
+      `;
     } else {
         copyrightTexts = copyTexts; 
         // 没超过7个字 则采用被复制的内容。
