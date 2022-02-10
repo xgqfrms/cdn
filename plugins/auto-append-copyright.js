1643c1ec90644f78e51495ed5f73343d51384158
xgqfrms <xgqfrms@outlook.com>
2022-02-11 00:26:51 +0800

    copyYear

--- plugins/auto-append-copyright.js
+++ plugins/auto-append-copyright.js
@@ -24,6 +24,7 @@ document.body.oncopy = event => {
     event.preventDefault(); 
         // 阻止默认事件 
     const copyTexts = window.getSelection(0).toString();
+    const copyYear = new Date().getFullYear();
     // 保存复制文字
     let copyrightTexts = ``;
     // 复制很少的文字则不添加版权信息，超过一定长度的文字就添加版权信息
@@ -34,7 +35,7 @@ ${copyTexts}
 链接：${window.location.href ?? 'https://www.cnblogs.com/xgqfrms/'}
 来源：${window.location.origin}
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-            ©xgqfrms 2012-2021
+            ©xgqfrms 2012-${copyYear}
 ${window.location.host} 发布文章使用：只允许注册用户才可以访问！
      原创文章，版权所有©️xgqfrms, 禁止转载 🈲️，侵权必究⚠️！
       `;
