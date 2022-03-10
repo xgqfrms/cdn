38f25547bbac1095ec411e3281c42738f370f8ed
xgqfrms <xgqfrms@outlook.com>
2022-03-10 13:09:21 +0800

    Create share-twitter.js

---
+++ sdk/share-twitter.js
@@ -0,0 +1,3 @@
+// https://twitter.com/intent/tweet?url=https%3A%2F%2Fstackoverflow.com%2Fa%2F64754401%2F5934465%3Fstw%3D2&ref=twitbtn&text=Answer%3A%20How%20to%20create%20an%20array%20containing%201...N
+
+
