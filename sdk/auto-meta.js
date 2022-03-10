1b6a3c5ab5a954ce483941b10ebadf5c8f65bda3
xgqfrms <xgqfrms@outlook.com>
2022-03-10 14:30:56 +0800

    Create auto-meta.js

---
+++ sdk/auto-meta.js
@@ -0,0 +1,31 @@
+const autoGeneratorMeta = (options = {
+    uid: 'h1.postTitle',
+    author: 'xgqfrms',
+    postId: 'cnblogs_post_body',
+  }) => {
+  const {uid, author, postId} = options;
+  // 一键分享
+  const h1 = document.querySelector(`${uid}`);
+  const text = window.encodeURIComponent(h1?.innerText ?? '暂无文章标题');
+  const post = document.querySelector(`[id="${postId}"]`);
+  const content = post.innerText.slice(0, 200) ?? '暂无文章标内容';
+  // const content = window.encodeURIComponent(post.innerText.slice(0, 200) ?? '暂无文章标内容');
+  const domain = window.location.origin;
+  const site = window.location.href;
+  const flag = `https://s11.flagcounter.com/count2/QIXi/bg_000000/txt_00FF00/border_FF00FF/columns_3/maxflags_12/viewers_0/labels_1/pageviews_1/flags_0/percent_1/`;
+  const img = post.querySelector('img')?.src ?? flag;
+  // const imgSrc = window.encodeURIComponent(post.querySelector('img')?.src ?? flag);
+  const meta = `
+    <meta name="twitter:card" content="${img}">
+    <meta name="twitter:image" content="${img}">
+    <meta name="twitter:title" content="${text}">
+    <meta name="twitter:description" content="${content}">
+    <meta name="twitter:domain" content="${domain}">
+    <meta name="twitter:site" content="@${site}">
+    <meta name="twitter:creator" content="@${author}">
+  `;
+  const head = document.querySelector('head');
+  head.insertAdjacentHTML('beforeend', meta);
+}
+
+autoGeneratorMeta();
