425efab3d0b59e64aad754aa780e8ce06f9781dd
xgqfrms <xgqfrms@outlook.com>
2022-04-22 14:22:16 +0800

    Update app.js

--- geolocation-api/app.js
+++ geolocation-api/app.js
@@ -13,6 +13,8 @@ function fetchVideoStream(videoEle) {
         videoEle.play();
         console.log('videoEle =', videoEle);
         console.log('stream =', stream);
+        console.log('\n videoEle', videoEle);
+        console.log('✅✅ videoEle.videoHeight,  videoEle.height =', videoEle.videoHeight,  videoEle.height);
         // ???
         // videoStream = stream;
         // Initiate frame capture - Processing Layer.
@@ -32,10 +34,21 @@ function fetchVideoStream(videoEle) {
  */
 
 async function captureFrames(videoEle, canvasEle, canvasCtx) {
+  console.log('\n ❌ videoEle', videoEle);
+  console.log('videoEle.readyState =', videoEle.readyState);
+  console.log('videoEle.HAVE_ENOUGH_DATA =', videoEle.HAVE_ENOUGH_DATA);
   if (videoEle.readyState === videoEle.HAVE_ENOUGH_DATA) {
-    const canvasHeight = (canvasEle.height = videoEle.videoHeight);
-    const canvasWidth = (canvasEle.width = videoEle.videoWidth);
+    // const canvasHeight = (canvasEle.height = videoEle.videoHeight);
+    // const canvasWidth = (canvasEle.width = videoEle.videoWidth);
+    // const canvasHeight = videoEle.videoHeight;
+    // const canvasWidth = videoEle.videoWidth;
+    console.log('\n videoEle', videoEle);
+    console.log('✅ videoEle.videoHeight,  videoEle.height =', videoEle.videoHeight,  videoEle.height);
+    console.log('canvasEle.height, canvasEle.width', canvasEle.height, canvasEle.width);
+    const canvasHeight = videoEle.height;
+    const canvasWidth = videoEle.width;
     canvasCtx.drawImage(videoEle, 0, 0, canvasWidth, canvasHeight);
+    console.log('canvasCtx', canvasCtx, canvasWidth, canvasHeight);
     // Transfer the `canvasEle` to the decoder for barcode detection.
     const result = await decodeBarcode(canvasEle);
     console.log('result ', result);
@@ -105,6 +118,11 @@ const app = (videoId = 'video', canvasId = 'canvas') => {
   const videoEle = document.getElementById(videoId);
   const canvasEle = document.getElementById(canvasId);
   const canvasCtx = canvasEle.getContext('2d');
+  // width="400" height="300"
+  // canvasCtx.width  = 400;
+  // canvasCtx.height = 300;
+  // canvasCtx.canvas.width  = window.innerWidth;
+  // canvasCtx.canvas.height = window.innerHeight;
   // console.log('videoEle =', videoEle);
   // console.log('canvasEle, canvasCtx =', canvasEle, canvasCtx);
   fetchVideoStream(videoEle);
