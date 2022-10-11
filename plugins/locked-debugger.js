963d5c99012574ed5150d1b90928fc9dc9fcba63
xgqfrms <xgqfrms@outlook.com>
2022-10-11 10:16:32 +0800

    Update locked-debugger.js

--- plugins/locked-debugger.js
+++ plugins/locked-debugger.js
@@ -12,7 +12,47 @@
 
 // 无限循环，锁死 debugger
 
-(function anonymous() {
-   debugger;
-   anonymous();
+// (function anonymous() {
+//    debugger;
+//    anonymous();
+// })();
+
+// 尾调用优化 / 尾递归优化
+
+// "use strict";
+// (function anonymous() {
+//    "use strict";
+//    debugger;
+//     // 
+//    return anonymous();
+// })();
+
+"use strict";
+(() => {
+  "use strict";
+  const anonymous = () => {
+    "use strict";
+    debugger;
+    // window.requestAnimationFrame(anonymous);
+    return anonymous();
+  };
+  // inint
+  anonymous();
 })();
+
+
+
+/*
+
+
+const callback = () => {
+  window.requestAnimationFrame(callback);
+}
+// inint
+callback();
+
+
+// https://www.cnblogs.com/xgqfrms/p/13065885.html
+*/
+
+
