8d506c309c92395a367809856b18ebb62893abfe
xgqfrms <xgqfrms@outlook.com>
2024-10-30 10:36:41 +0800

    Create github-page-to-github-repo.js

---
+++ github/github-page-to-github-repo.js
@@ -0,0 +1,36 @@
+const autoConvertPageToRepo = (uri = ``) => {
+  if(!globalThis.window) {
+    throw new Error(`❌ 当前的 js 运行环境不支持 Web API!`)
+  }
+  let url = ``;
+  try {
+    if(!uri) {
+      // in io page ✅
+      const host = window.location.host.split(`.`)[0];  
+      const protocol = window.location.protocol;
+      const pathname = window.location.pathname;
+      url = `${protocol}//github.com/${host}${pathname}`;
+    } else {
+      // URL parser，ulr string => URL 🚀
+      const location = new URL(uri);
+      const host = location.host.split(`.`)[0];  
+      const protocol = location.protocol;
+      const pathname = location.pathname;
+      url = `${protocol}//github.com/${host}${pathname}`;
+    }
+    console.log(`✅ repo url =`, url)
+  } catch (error) {
+    console.error(`❌ generate repo url error`, error)
+  }
+  return url;
+}
+
+/*
+
+// test cases
+autoConvertPageToRepo();
+// autoConvertPageToRepo(`https://txstc55.github.io/ugly-avatar/`);
+// ✅ repo url = https://github.com/txstc55/ugly-avatar/
+
+
+  */
