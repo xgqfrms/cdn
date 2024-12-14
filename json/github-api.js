9670a88af362d94ffc73205dbc4587582c25c9da
xgqfrms <xgqfrms@outlook.com>
2024-12-14 23:05:16 +0800

    Update github-api.js
    process.env.NODE_ENV

--- json/github-api.js
+++ json/github-api.js
@@ -3,13 +3,13 @@ let username = `xgqfrms-GitHub`;
 
 fetch(`https://api.github.com/users/${username}/${repo}`,{
     data: {
-        client_id: '08ecc2f68d922f18800e',
-        client_secret: '5846d428b5340812b76c9637eceaee979340b922'
+         client_id: process.env.NODE_ENV_ID,
+         client_secret: process.env.NODE_ENV_SECRET,
     }
 })
 .then((response) => response.json())
 .then((json)=> {
-    console.log(`json = ${json}`);
+    console.log(`json =`, json);
     return repos = json;
 })
 .then((repos)=>{
@@ -22,10 +22,51 @@ fetch(`https://api.github.com/users/${username}/${repo}`,{
     }
 });
 
+
+
 // https://api.github.com/users/xgqfrms
 
 // https://api.github.com/users/xgqfrms/react2 ???
 
+/*
+
+/**
+ * [nct: fetch data]
+ * @author: xgqfrms
+ * @date: 2017-06-12
+ */
+
+const fetch = require('node-fetch');
+const showdata = require('./showdata');
+
+// NODE_ENV_ID = ``;
+// NODE_ENV_SECRET = ``;
+
+const nct = (username, repo) => {
+    let url = `https://api.github.com/repos/${username}/${repo}/commits`;
+    let options = {
+        data: {
+            client_id: process.env.NODE_ENV_ID,
+            client_secret: process.env.NODE_ENV_SECRET,
+        },
+    };
+    fetch(url, options)
+    .then((res) => res.json())
+    .then((repos)=>{
+        showdata(repos);
+    })
+    .catch(err) => {
+      console.log(`error info =`, err);
+    };
+};
+
+module.exports = nct;
+
+
+https://github.com/xgqfrms-GitHub/Node-CLI-Tools/blob/master/libs/index.js
+
+
+*/
 
 
 
